cda59720f0c02906a91b49dddc60067dfd0fef9b
Iso Thiak <falliso469@gmail.com>
2018-12-30 17:35:18 +0000

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -15,19 +16,19 @@ app.use(express.static(__dirname + '/public'));
 // });
 
 
-app.use((req,res, next) => {
-    var now = new Date().toString();
-    var log = `${now}: ${req.method} ${req.url}`;  
+// app.use((req,res, next) => {
+//     var now = new Date().toString();
+//     var log = `${now}: ${req.method} ${req.url}`;  
 
-    console.log(log);
-    fs.appendFileSync('server.log', log + '\n'); 
-    next();
-});
+//     console.log(log);
+//     fs.appendFileSync('server.log', log + '\n'); 
+//     next();
+// });
 
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs');
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -63,8 +64,8 @@ app.get('/bad', (req, res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
 });
 
 
@@ -72,4 +73,4 @@ app.listen(3000, () => {
 // Express est une infrastructure d'applications Web Node.js minimale et flexible qui fournit un 
 // ensemble robuste de fonctionnalités pour les applications Web et mobiles
 // Avec une myriade de méthodes utilitaires HTTP et de middleware à votre disposition, 
-// la création d'une API robuste est simple et rapide. 
+// la création d'une API robuste est simple et rapide.  
